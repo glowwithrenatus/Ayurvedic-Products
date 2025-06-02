295302b01e909a1f024a4a1adf031ff77a5555b8
glowwithrenatus <wellnesswithrenatus@gmail.com>
2025-06-02 20:44:25 +0530

    Update script.js
    code updated

--- script.js
+++ script.js
@@ -7,25 +7,85 @@ const products = [
     id: 1,
     name: "FACEWASH",
     priceUSD: 3.91,
-    images: ["images/p11.jpg", "images/p12.jpg"]
+    images: ["images/p11.jpg", "images/p12.jpg", "images/p13.jpg"]
   },
   {
     id: 2,
     name: "FACE SCRUB",
     priceUSD: 3.91,
-    images: ["images/P21.jpg", "images/p22.jpg", "images/p23.jpg"]
+    images: ["images/p21.jpg", "images/p22.jpg", "images/p23.jpg"]
   },
   {
     id: 3,
     name: "SUN SCREEN LOTION",
     priceUSD: 3.91,
-    images: ["images/p31.jpg", "images/p32.jpg", "images/p33.jpg", "images/p34.jpg","images/p35.jpeg"]
+    images: ["images/p31.jpg", "images/p32.jpg", "images/p33.jpg", "images/p34.jpg"]
   },
   {
     id: 4,
     name: "BODY MIST",
     priceUSD: 5.50,
     images: ["images/p41.jpg", "images/p42.jpg", "images/p43.jpg", "images/p44.jpg", "images/p45.jpg", "images/p46.jpg", "images/p47.jpg", "images/p48.jpg"]
+  },
+  {
+    id: 5,
+    name: "FACE MOISTURIZER",
+    priceUSD: 3.91,
+    images: ["images/p51.jpg", "images/p52.jpg"]
+  },
+  {
+    id: 6,
+    name: "ALOE VERA GEL",
+    priceUSD: 3.91,
+    images: ["images/p61.jpg", "images/p62.jpg"]
+  },
+  {
+    id: 7,
+    name: "NOVASTEEN",
+    priceUSD: 5.39,
+    images: ["images/p71.jpg", "images/p72.jpg", "images/p73.jpg", "images/p74.jpg"]
+  },
+  {
+    id: 8,
+    name: "NIGHT CREAM",
+    priceUSD: 7.17,
+    images: ["images/p81.jpg", "images/p82.jpg"]
+  },
+  {
+    id: 9,
+    name: "FACE SERUM",
+    priceUSD: 5.69,
+    images: ["images/p91.jpg", "images/p92.jpg", "images/p93.jpg"]
+  },
+  {
+    id: 10,
+    name: "HAIR SERUM",
+    priceUSD: 5.48,
+    images: ["images/p101.jpg", "images/p102.jpg", "images/p103.jpg"]
+  },
+  {
+    id: 11,
+    name: "ROSE WATER",
+    priceUSD: 11.70,
+    images: ["images/p111.jpg", "images/p112.jpg", "images/p113.jpg", "images/p114.jpg"]
+  },
+  {
+    id: 12,
+    name: "AP LOTION",
+    priceUSD: 4.85,
+    images: ["images/p121.jpg", "images/p122.jpg"]
+  },
+  {
+    id: 13,
+    name: "FACE TONER",
+    priceUSD: 3.91,
+    images: ["images/p131.jpg"]
+  },
+  {
+    id: 14,
+    name: "HAIR CARE CREAM",
+    priceUSD: 5.59,
+    images: ["images/p141.jpg"]
   }
 ];
 
@@ -168,7 +228,7 @@ function generateCartMessage() {
 
 function sendMessageViaWhatsApp() {
   closeModal();
-  const phoneNumber = "918273984482"; 
+  const phoneNumber = "918273984482";
   const message = generateCartMessage();
   const url = `https://wa.me/${phoneNumber}?text=${message}`;
   window.open(url, "_blank");
@@ -176,7 +236,7 @@ function sendMessageViaWhatsApp() {
 
 function sendMessageViaInstagram() {
   closeModal();
-  const igUsername = "glow_with_renatus"; 
+  const igUsername = "glow_with_renatus";
   const message = generateCartMessage();
   const url = `https://ig.me/m/glow_with_renatus`;
   window.open(url, "_blank");
